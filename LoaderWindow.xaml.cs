cfdc7bc06d1909a0a31e6616f0d885ec00d42ba3
ajayraho <ajayraho@ymail.com>
2026-03-22 14:07:41 +0530

    LoaderWindow: UpdateStatus/ShowCancelAction API, CancelAIRequested event

--- LoaderWindow.xaml.cs
+++ LoaderWindow.xaml.cs
@@ -1,11 +1,34 @@
+using System;
 using System.Windows;
+using System.Windows.Input;
+
 namespace ScreenRecApp
 {
     public partial class LoaderWindow : Window
     {
+        public event Action CancelAIRequested;
+
         public LoaderWindow()
         {
             InitializeComponent();
+        }
+
+        public void UpdateStatus(string status)
+        {
+            Dispatcher.Invoke(() => StatusText.Text = status);
+        }
+
+        public void ShowCancelAction(bool show)
+        {
+            Dispatcher.Invoke(() => CancelActionText.Visibility = show ? Visibility.Visible : Visibility.Collapsed);
+        }
+
+        private void CancelAction_Click(object sender, MouseButtonEventArgs e)
+        {
+            CancelActionText.Text = "Cancelling...";
+            CancelActionText.Foreground = new System.Windows.Media.SolidColorBrush((System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#6c7086"));
+            CancelActionText.Cursor = System.Windows.Input.Cursors.Arrow;
+            CancelAIRequested?.Invoke();
         }
     }
 }
